1964d18bd9e6a08fe16408fc7079cf25e485ecb6
chunza2542 <hello@thechun.dev>
2025-02-10 15:48:50 +0700

    fix bug: append btn

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -36,26 +36,40 @@ export const Canvas: React.FC = () => {
   };
 
   const handleTextSelectionChange = (ids: string[]) => {
+    // Check if label already exists
+    const existingLabel = document.getElementById("open-custom-fonts-modal");
+    if (existingLabel) {
+      // Remove existing label and create a new one
+      existingLabel.remove();
+    }
+
     // add custom fonts to the popup
+    const elements = excalidrawAPIRef.current
+      ?.getSceneElements()
+      .filter((e) => ids.includes(e.id));
+    const isAllText = elements?.every((e) => e.type === "text");
+    if (!isAllText) return;
+
     const fontFamilyPopup = document.querySelector(
       "div.Stack.Stack_vertical.App-menu_top__left fieldset:nth-child(3) > div"
     );
     if (!fontFamilyPopup) return;
 
-    const codeLabel = document.createElement("label");
-    codeLabel.title = "Custom Fonts";
-    codeLabel.innerHTML = `
+    const label = document.createElement("label");
+    label.id = "open-custom-fonts-modal";
+    label.title = "Custom Fonts";
+    label.innerHTML = `
         <input type="radio" name="custom-fonts">
         <svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 24 24" class="" fill="none" stroke-width="2" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><g stroke-width="1.5"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="20" x2="7" y2="20"></line><line x1="14" y1="20" x2="21" y2="20"></line><line x1="6.9" y1="15" x2="13.8" y2="15"></line><line x1="10.2" y1="6.3" x2="16" y2="20"></line><polyline points="5 20 11 4 13 4 20 20"></polyline></g></svg>
       `;
 
-    codeLabel.addEventListener("click", (e) => {
+    label.addEventListener("click", (e) => {
       e.preventDefault();
       e.stopPropagation();
       openModal("custom-fonts-modal");
     });
 
-    fontFamilyPopup.appendChild(codeLabel);
+    fontFamilyPopup.appendChild(label);
   };
 
   useEffect(() => {
