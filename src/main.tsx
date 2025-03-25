ce8f46cf9ece071b985bc0d84dfcc8a3f45ea675
chunza2542 <hello@thechun.dev>
2025-03-25 13:41:07 +0700

    set EXCALIDRAW_ASSET_PATH

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+declare global {
+  interface Window {
+    EXCALIDRAW_ASSET_PATH: string;
+  }
+}
+window.EXCALIDRAW_ASSET_PATH = "/excalidraw";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
